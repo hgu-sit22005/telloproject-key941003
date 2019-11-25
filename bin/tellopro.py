8b55d46069e126cc4030c4cbb69aabf8dfa7b576
key941003 <21300507@handong.edu>
2019-11-25 23:18:27 +0900

    submission

--- bin/tellopro.py
+++ bin/tellopro.py
@@ -12,6 +12,12 @@ command_lst.append(TelloPro.get_instance('up', 50))
 #command_lst.append(TelloPro.get_instance('down', 50))
 #for i in range(4):
 	#command_lst.append(TelloPro.get_instance('flip',i))
+#command_lst.append(TelloPro.get_instance('left', 50))
+#command_lst.append(TelloPro.get_instance('right', 50))
+#command_lst.append(TelloPro.get_instance('forward', 50))
+#command_lst.append(TelloPro.get_instance('back', 50))
+#command_lst.append(TelloPro.get_instance('cw', 50))
+#command_lst.append(TelloPro.get_instance('ccw', 50))
 
 command_lst.append(TelloPro.get_instance('land', -1))
 for command in command_lst:
